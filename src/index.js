a2b768982f45319cecb7b526fa4c6249fb5a4a62
Vitalii <ukrainskiy.vitalii@gmail.com>
2022-02-23 23:58:16 +0200

    feat: add check method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(input, config) {
+    let basket = [];
+    let brackets = Object.fromEntries(config);
+
+    input.split('').forEach((elem) => {
+        let last = basket[basket.length - 1];
+        (brackets[last] === elem) ? basket.pop(): basket.push(elem)
+    });
+
+    return basket.length === 0;
 }
